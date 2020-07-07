609ca83e7fb9b058a122e3921b4482bb1add9b7a
Natalia Galán <n8galan@gmail.com>
2020-07-07 21:59:01 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,6 @@
 #returns that same sentence with each word reversed in place
 
 def reverse_each_word(sentence)
-
   reversed_sentence = sentence.split.collect { |word| "#{word.reverse}" }
   reversed_sentence.join(' ')
 end
