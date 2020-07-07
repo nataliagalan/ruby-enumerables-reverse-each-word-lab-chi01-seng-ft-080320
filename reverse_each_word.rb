1420ccfe8fe5630585009354ced09e81ee45c355
Natalia Galán <n8galan@gmail.com>
2020-07-07 21:58:45 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,6 @@
 require 'pry'
-
+# takes in a string argument of a sentence and 
+returns that same sentence with each word reversed in place
 def reverse_each_word(sentence)
 
   reversed_sentence = sentence.split.collect { |word| "#{word.reverse}" }
